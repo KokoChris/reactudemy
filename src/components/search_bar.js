ca7b518e1a8e211f2eef3d27c3eb60f334e47ecf
Chris Kokolios <kokolios1986@yahoo.gr>
2016-07-19 23:11:41 +0300

    coding along

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 
-const SearchBar = () => {
-	return <input />
-};
+class SearchBar extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = { term: ''};
+	}
+    render() {
+
+        return (
+        <div>	
+        	<input 
+        	value= {this.state.term}
+        	onChange = { (event) => this.setState({ term: event.target.value}) }/>
+        </div>
+        );
+    }
+
+
+
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCsVXxsB1Z6CTXcvklA193Re68E8BMxSoM';
 
-const App = () => {
-	return  (
+
+
+
+class App  extends Component  {
+	constructor(props) {
+		super(props); 
+
+		this.state = {videos: [] };
+		
+
+		YTSearch({key:API_KEY, term:"surfboards"}, (videos) => { this.setState({videos})})
+	}
+	render()  {
+	return(
 	<div>  
 		<SearchBar />
 	</div> 
 	);
+	}
 }
 
 
