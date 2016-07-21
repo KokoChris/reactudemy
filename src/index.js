3b313f422efaba4df47e0c721b00f0712f2662a4
kokoChris <kokolios1986@yahoo.gr>
2016-07-21 20:25:39 +0300

    coding along

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCsVXxsB1Z6CTXcvklA193Re68E8BMxSoM';
 
@@ -15,12 +17,14 @@ class App  extends Component  {
 		this.state = {videos: [] };
 		
 
-		YTSearch({key:API_KEY, term:"surfboards"}, (videos) => { this.setState({videos})})
+		YTSearch({key:API_KEY, term:"winterSC"}, (videos) => { this.setState({videos})})
 	}
 	render()  {
 	return(
 	<div>  
 		<SearchBar />
+		<VideoDetail video={this.state.videos[0]} />
+		<VideoList videos={this.state.videos}/>
 	</div> 
 	);
 	}
